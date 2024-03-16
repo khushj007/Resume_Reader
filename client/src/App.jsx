2efef00b6170ffcc3a9e17862f35c67efa703e35
khush <khushjoshi007@gmail.com>
2024-03-16 23:50:57 +0530

    frontend deign modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Landingpage from "./pages/Landingpage/Landingpage";
-import Homepage from "./pages/Homepage/Homepage";
+import Homepage from "./pages/Homepage/Homepage.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<Landingpage />}></Route>
-        <Route path={"/homepage"} element={<Homepage />}></Route>
-      </Routes>
+    <>
+      <Homepage />
       <ToastContainer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -4,42 +4,52 @@ import axios from "axios";
 import Loader2 from "../../components/Loader2/Loader2";
 import Loader from "../../components/Loader/Loader";
 import { toast } from "react-toastify";
+import Logo from "../../assets/logo.svg";
+import File from "../../assets/file.svg";
+import Submit from "../../assets/submit.png";
+import System from "../../assets/system.jpeg";
+import Profile from "../../assets/profile.png";
+import { v4 as uuidv4 } from "uuid";
 const Homepage = () => {
-  const [query, setQuery] = useState([""]);
-  const [response, setResponse] = useState(["Ask me anything ?"]);
+  const FileUploaded = useRef(false);
+  const [Filename, setFileName] = useState("");
   const [question, setQuestion] = useState([""]);
   const [loading, setLoading] = useState(false);
   const [loading1, setLoading1] = useState(false);
-  const maxLength = Math.max(query.length, response.length);
+  const [rq, setRq] = useState(["Ask me anything ?"]);
   const chatsRef = useRef(null);
 
   // automatically scroll down the chats
   const scrollToBottom = () => {
     if (chatsRef?.current?.scrollHeight) {
       chatsRef.current.scrollTop = chatsRef?.current?.scrollHeight;
+      console.log(chatsRef?.current?.scrollHeight);
     }
   };
 
   useEffect(() => {
     scrollToBottom();
-  }, [maxLength, response, query]);
-
+  }, [rq.length]);
   //  function to send query and receive answers
   async function handleSubmit() {
     // validation question should not be empty
-    if (!question) {
-      toast.error("provide query");
-      return;
-    }
-    setLoading(true);
     try {
-      setQuery((prevQueries) => [...prevQueries, question]);
+      setLoading(true);
+      if (!question) {
+        toast.error("provide query");
+        return;
+      }
+      if (!FileUploaded.current) {
+        toast.error("please upload resume first");
+        return;
+      }
 
+      setRq((prevvalue) => [...prevvalue, question]);
       const res = await axios.post("https://resume-reader.onrender.com/query", {
         query: question,
       });
       if (res.status === 200) {
-        setResponse((prevResponses) => [...prevResponses, res.data.response]);
+        setRq((prevvalue) => [...prevvalue, res.data.response]);
       } else {
         toast.error("No data received from the server");
       }
@@ -56,6 +66,8 @@ const Homepage = () => {
     try {
       setLoading1(true);
       const selectedFile = e.target.files[0];
+      setFileName(e.target.files[0].name);
+      setRq(["Ask me anything ?"]);
 
       const formData = new FormData();
       formData.append("file", selectedFile);
@@ -64,6 +76,9 @@ const Homepage = () => {
         "https://resume-reader.onrender.com/pdf",
         formData
       );
+
+      console.log("respone", response);
+      FileUploaded.current = response.status == 200 ? true : false;
     } catch (error) {
       console.log("error", error.message);
     } finally {
@@ -78,27 +93,45 @@ const Homepage = () => {
   return (
     <div className="homepage">
       <div className="navbar">
-        <h2>Read Resume</h2>
-        <input
-          style={{ display: "none" }}
-          id="file"
-          type="file"
-          onChange={changePdf}
-          accept=".pdf"
-        />
-        <label htmlFor="file" className="unp">
-          Change PDF
-        </label>
+        <img src={Logo} alt="logo" className="logo" />
+        <div className="rnav">
+          <input
+            style={{ display: "none" }}
+            id="file"
+            type="file"
+            onChange={changePdf}
+            accept=".pdf"
+          />
+          {Filename ? <img className="fimg" src={File} alt="file" /> : null}
+          {Filename ? <p className="fname">{Filename}</p> : null}
+          <label htmlFor="file" className="unp">
+            <span>&#x2295;</span> Upload PDF
+          </label>
+          <label htmlFor="file" className="unp2">
+            &#x2295;
+          </label>
+        </div>
       </div>
       <div className="page">
         {/* display both response and chats in a chat style */}
         <div className="chats" ref={chatsRef}>
-          {Array.from({ length: maxLength }).map((_, index) => (
-            <div key={index} className="chat">
-              <div className="query">{query[index]}</div>
-              <div className="response">{response[index]}</div>
-            </div>
-          ))}
+          {rq.map((value, index) => {
+            if (index % 2 != 0) {
+              return (
+                <div key={uuidv4()} className="query">
+                  <img src={Profile} alt="profile" />
+                  <p>{value}</p>
+                </div>
+              );
+            } else {
+              return (
+                <div key={uuidv4()} className="response">
+                  <img src={System} alt="system" />
+                  <p>{value}</p>
+                </div>
+              );
+            }
+          })}
           {loading ? <Loader2 /> : null}
         </div>
       </div>
@@ -111,13 +144,16 @@ const Homepage = () => {
               setQuestion(e.target.value);
             }}
             type="text"
-            placeholder="Ask me anything "
-            className="askquery"
+            placeholder="Send a message..."
+            className="askquery_inp"
             value={question}
           />
-          <button className="submit" onClick={handleSubmit}>
-            Submit
-          </button>
+          <img
+            className="submit"
+            src={Submit}
+            alt="submit"
+            onClick={handleSubmit}
+          />
         </div>
       </div>
     </div>
